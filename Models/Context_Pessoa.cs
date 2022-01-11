b66bf5f501c7c935490454811fc13645e568464b
Drake-Montgomery <eder_vip@outlook.com>
2022-01-10 23:59:22 -0300

    Add project files.

---
+++ Models/Context_Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Abar.Models
+{
+    public class Context_Pessoa : DbContext
+    {
+        public Context_Pessoa(DbContextOptions<Context_Pessoa> options) : base(options)
+        {
+        }
+       public DbSet<Pessoa> ? Pessoa { get; set; }
+       public DbSet<Login>? Login { get; set; }
+    }
+}
